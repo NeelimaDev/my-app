8f0c9f8be16c52f6b8c827612d6c35f6972c30b0
NeelimaDev <neelima@finiteloop.io>
2023-04-27 16:36:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+
 
 function App() {
+  const [users, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/cars')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table border="1">
+      <TableHead>
+        <TableRow>
+          <TableCell>Company</TableCell>
+          <TableCell>Car</TableCell>
+          <TableCell>Engine</TableCell>
+          <TableCell>Geartype</TableCell>
+          <TableCell>color</TableCell>
+          <TableCell>MaxSpeed</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map(row => (
+          <TableRow key={row.id}>
+            <TableCell>{row.Company}</TableCell>
+            <TableCell>{row.Car}</TableCell>
+            <TableCell>{row.Engine}</TableCell>
+            <TableCell>{row.Geartype}</TableCell>
+            <TableCell>{row.Color}</TableCell>
+            <TableCell>{row.MaxSpeed}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
-
 export default App;
